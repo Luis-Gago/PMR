20b587aac2b87ee3a6d714c8c27df72c15ff151c
Luis Gago <luisgago@pa-dhcp-10-120-230-217.gp-vpn.northwestern.private>
2023-09-11 22:41:35 -0500

    final edits

--- Assets/Scripts/PipeMiddleScript.cs
+++ Assets/Scripts/PipeMiddleScript.cs
@@ -7,13 +7,20 @@ public class PipeMiddleScript : MonoBehaviour
 {
     public LogicScript logic;
     private Scene currentScene; // Declare currentScene as a class-level variable
+    private BirdScript birdScript; // Reference to the BirdScript component
+
+    private bool isResettingPosition = false; // Flag to check if the position is being reset
+    private Vector3 resetStartPosition; // Starting position for resetting
+
+    public float resetDuration = 1.0f; // Duration for the reset animation (you can adjust this)
 
     // Start is called before the first frame update
     void Start()
     {
         logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+        birdScript = FindObjectOfType<BirdScript>(); // Get the BirdScript component
 
-         // Get the currently active scene
+        // Get the currently active scene
         currentScene = SceneManager.GetActiveScene();
 
         // Print the name of the current scene
@@ -23,11 +30,11 @@ public class PipeMiddleScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (logic.playerScore == 10 && currentScene.name == "Level1")
+        if (logic.playerScore == 25 && currentScene.name == "Level1")
         {
             logic.levelTwo();
         }
-        else if (logic.playerScore == 20 && currentScene.name == "Level2")
+        else if (logic.playerScore == 50 && currentScene.name == "Level2")
         {
             logic.levelThree();
         }
@@ -38,7 +45,36 @@ public class PipeMiddleScript : MonoBehaviour
         if (collision.gameObject.layer == 3)
         {
             logic.addScore(1);
+
+            // Check if bird's x position is between -4 and -9
+            if (birdScript.transform.position.x >= -11f && birdScript.transform.position.x <= -7f)
+            {
+                // Start the reset position coroutine if it's not already running
+                if (!isResettingPosition)
+                {
+                    resetStartPosition = birdScript.transform.position;
+                    StartCoroutine(ResetBirdPosition());
+                }
+            }
         }
-        
+    }
+
+    IEnumerator ResetBirdPosition()
+    {
+        isResettingPosition = true;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < resetDuration)
+        {
+            birdScript.transform.position = Vector3.Lerp(resetStartPosition, Vector3.zero, elapsedTime / resetDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        // Ensure the final position is exactly (0, y, z)
+        birdScript.transform.position = new Vector3(0f, birdScript.transform.position.y, birdScript.transform.position.z);
+
+        isResettingPosition = false;
     }
 }
+
